8b7087ff20bf366e8aec2faf2c3e3e7bdfb81815
SoniaS4nt <soniachaparro21@gmail.com>
2022-02-03 20:32:06 +0100

    change PORT index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import app from "./app";
-import "./database";
+import "./utils/mongoose";
 
-app.listen(3000);
-console.log("Server on port", 3000);
+app.listen(app.get("port"));
+console.log(`server on port ${app.get("port")}`);
